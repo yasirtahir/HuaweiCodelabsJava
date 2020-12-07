85990d424d278bc01457ecc0cd271b2a7cf08fc9
CHINA\y00591928 <yasir.tahir@huawei.com>
2020-12-07 09:28:29 +0300

    - Added Crash Kit

--- HuaweiCodelabs/app/src/main/java/com/yasir/huaweicodelabs/Utilities/AppLog.java
+++ HuaweiCodelabs/app/src/main/java/com/yasir/huaweicodelabs/Utilities/AppLog.java
@@ -2,22 +2,26 @@ package com.yasir.huaweicodelabs.Utilities;
 
 import android.util.Log;
 
-import com.yasir.huaweicodelabs.Utilities.AppConstant;
+import com.huawei.agconnect.crash.AGConnectCrash;
 
 public class AppLog {
 
     public static void Debug(String tag, String msg) {
         if (isEmptyOrNull(msg))
             return;
-        if (AppConstant.DEBUG)
+        if (AppConstant.DEBUG) {
             Log.d(tag, msg);
+            AGConnectCrash.getInstance().log(Log.DEBUG, msg);
+        }
     }
 
     public static void Error(String tag, String msg) {
         if (isEmptyOrNull(msg))
             return;
-        if (AppConstant.DEBUG)
+        if (AppConstant.DEBUG) {
             Log.e(tag, msg);
+            AGConnectCrash.getInstance().log(Log.ERROR, msg);
+        }
     }
 
     private static boolean isEmptyOrNull(String string) {
